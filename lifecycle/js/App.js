a851c130717e52021b12772da064cce4f600f77b
zhukovka <helen.zhukova@gmail.com>
2017-07-17 15:30:47 +0300

    toggleDetectorState added

--- lifecycle/js/App.js
+++ lifecycle/js/App.js
@@ -1,12 +1,23 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      detectorIsEnabled: false
+    };
   }
 
+  toggleDetectorState() {
+    this.setState({
+      detectorIsEnabled: !this.state.detectorIsEnabled
+    });
+  }
   render() {
     return (
       <div>
-        <WindowResizeDetector />
+        <button onClick={this.toggleDetectorState.bind(this)}>
+          {this.state.detectorIsEnabled ? "Выключить детектор" : "Включить детектор"}
+        </button>
+        {this.state.detectorIsEnabled ? <WindowResizeDetector /> : null}
       </div>
     );
   }
