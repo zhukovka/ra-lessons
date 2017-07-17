1509ddab1d73d8efbbc11babe31ece574506c746
zhukovka <helen.zhukova@gmail.com>
2017-07-17 15:31:59 +0300

    componentWillUnmount added

--- lifecycle/js/WindowResizeDetector.js
+++ lifecycle/js/WindowResizeDetector.js
@@ -2,10 +2,16 @@ class WindowResizeDetector extends React.Component {
   componentDidMount() {
     window.addEventListener('resize', this.windowResizeHandler);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.windowResizeHandler);
+  }
+
   windowResizeHandler() {
     console.log('Размер окна был изменен!');
     // ...логика обработки события
   }
+
   render() {
     return <div>Детектор включен</div>;
   }
