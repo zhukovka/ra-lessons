59852d669b3bba49f93a3bc46084edadbd1545b1
zhukovka <helen.zhukova@gmail.com>
2017-07-17 15:29:21 +0300

    old state removed

--- lifecycle/js/App.js
+++ lifecycle/js/App.js
@@ -1,18 +1,8 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      jsRate: 40,
-      javaRate: 90,
-      sharpRate: 30
-    }
   }
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
   render() {
     return (
       <div>
