815c32a76ed8d431a85428ba705e502fbf98f416
zhukovka <helen.zhukova@gmail.com>
2017-07-17 12:56:16 +0300

    initial

---
+++ lifecycle/js/App.js
@@ -0,0 +1,5 @@
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
---
+++ lifecycle/js/Calculator.js
@@ -0,0 +1,43 @@
+class Calculator extends React.Component {
+  constructor(...params) {
+    super(...params);
+    this.state = {
+      rubAmount: 0,
+      rate: 57,
+    };
+  }
+
+  calcUSDsum() {
+    return (this.state.rubAmount / this.state.rate).toFixed(4);
+  }
+
+  handleAmountChange(event) {
+    this.setState({
+      rubAmount: event.target.value,
+    });
+  }
+
+  render() {
+    const { rubAmount, rate } = this.state;
+    return (
+      <div>
+        <h3>Конвертер валют:</h3>
+        <div>
+          Текущий курс: {rate}
+        </div>
+        <div>
+          <span>Сумма в рублях: </span>
+          <input
+            type="text"
+            placeholder="Сумма в рублях"
+            onChange={this.handleAmountChange.bind(this)}
+            value={rubAmount}
+          />
+        </div>
+        <span>
+          Сумма в долларах: {this.calcUSDsum()}
+        </span>
+      </div>
+    );
+  }
+}
