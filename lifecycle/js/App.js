e977a98935819733adaff50621b5290a511f375b
zhukovka <helen.zhukova@gmail.com>
2017-07-17 14:26:38 +0300

    test async set state in componentWillMount

--- lifecycle/js/App.js
+++ lifecycle/js/App.js
@@ -1,5 +1,22 @@
-const App = () => (
-  <div>
-    <Calculator />
-  </div>
-);
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      test: "initial state"
+    }
+  }
+
+  componentWillMount(){
+    setTimeout(()=>{
+      this.setState({test:"async set state 0ms componentWillMount"});
+    }, 0);
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.test}
+      </div>
+    );
+  }
+}
