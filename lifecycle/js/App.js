7a2374861bd8d554d1c05bc24adfb839dd68ed07
zhukovka <helen.zhukova@gmail.com>
2017-07-17 15:28:26 +0300

    WindowResizeDetector added

--- lifecycle/js/App.js
+++ lifecycle/js/App.js
@@ -16,26 +16,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Популярность языков программирования</h2>
-        <div>
-          JavaScript:
-          <input type="text" name="jsRate" value={this.state.jsRate} onChange={this.handleChange.bind(this)} />
-        </div>
-        <div>
-          Java:
-          <input type="text" name="javaRate" value={this.state.javaRate} onChange={this.handleChange.bind(this)} />
-        </div>
-        <div>
-          C#:
-          <input type="text" name="sharpRate" value={this.state.sharpRate} onChange={this.handleChange.bind(this)} />
-        </div>
-        <CircleChart rates={[this.state.jsRate, this.state.javaRate,
-          this.state.sharpRate]} labels={[
-            'JavaScript',
-            'Java',
-            'C#'
-          ]}/>
-        </div>
-      );
-    }
+        <WindowResizeDetector />
+      </div>
+    );
   }
+}
---
+++ lifecycle/js/WindowResizeDetector.js
@@ -0,0 +1,12 @@
+class WindowResizeDetector extends React.Component {
+  componentDidMount() {
+    window.addEventListener('resize', this.windowResizeHandler);
+  }
+  windowResizeHandler() {
+    console.log('Размер окна был изменен!');
+    // ...логика обработки события
+  }
+  render() {
+    return <div>Детектор включен</div>;
+  }
+}
