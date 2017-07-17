a037bb320c7390a2f69bee5671f616c11cd22621
zhukovka <helen.zhukova@gmail.com>
2017-07-17 15:25:13 +0300

    refactor CircleChart with componentWillReceiveProps

--- lifecycle/js/App.js
+++ lifecycle/js/App.js
@@ -1,5 +1,41 @@
-const App = () => (
-  <div>
-    <CircleChart />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsRate: 40,
+      javaRate: 90,
+      sharpRate: 30
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h2>Популярность языков программирования</h2>
+        <div>
+          JavaScript:
+          <input type="text" name="jsRate" value={this.state.jsRate} onChange={this.handleChange.bind(this)} />
+        </div>
+        <div>
+          Java:
+          <input type="text" name="javaRate" value={this.state.javaRate} onChange={this.handleChange.bind(this)} />
+        </div>
+        <div>
+          C#:
+          <input type="text" name="sharpRate" value={this.state.sharpRate} onChange={this.handleChange.bind(this)} />
+        </div>
+        <CircleChart rates={[this.state.jsRate, this.state.javaRate,
+          this.state.sharpRate]} labels={[
+            'JavaScript',
+            'Java',
+            'C#'
+          ]}/>
+        </div>
+      );
+    }
+  }
--- lifecycle/js/CircleChart.js
+++ lifecycle/js/CircleChart.js
@@ -3,19 +3,10 @@ class CircleChart extends React.Component {
     super(props);
     this.data = {
       datasets: [{
-        data: [40, 90, 30],
+        data: props.rates,
         backgroundColor: ['rgb(255, 205, 86)', 'rgb(153, 102, 255)', 'rgb(75, 192, 192)']
-      }], labels: [
-        'JavaScript',
-        'Java',
-        'C#'
-      ]
+      }], labels: props.labels
     };
-    this.state = {
-      jsRate: 40,
-      javaRate: 90,
-      sharpRate: 30
-    }
   }
   componentDidMount() {
     this.chart = new Chart("myChart", {
@@ -28,37 +19,14 @@ class CircleChart extends React.Component {
       [event.target.name]: event.target.value
     });
   }
-  componentDidUpdate() {
-    this.updateChart();
+  componentWillReceiveProps(newProps) {
+    this.updateChart(newProps.rates);
   }
-  updateChart() {
-    this.chart.data.datasets[0].data = [
-      this.state.jsRate,
-      this.state.javaRate,
-      this.state.sharpRate
-    ];
+  updateChart(rates) {
+    this.chart.data.datasets[0].data = rates;
     this.chart.update();
   }
   render() {
-    return (
-      <div>
-        <h2>Популярность языков программирования</h2>
-        <div>
-          JavaScript:
-          <input type="text" name="jsRate" value={this.state.jsRate}
-            onChange={this.handleChange.bind(this)} />
-          </div> <div>
-            Java:
-            <input type="text" name="javaRate" value={this.state.javaRate}
-              onChange={this.handleChange.bind(this)} />
-            </div>
-            <div>
-              C#:
-              <input type="text" name="sharpRate" value={this.state.sharpRate}
-                onChange={this.handleChange.bind(this)} />
-              </div>
-              <canvas id="myChart" width="100" height="100" />
-            </div>
-          );
-        }
-      }
+    return (<canvas id="myChart" width="100" height="100" />);
+  }
+}
