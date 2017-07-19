65429e378e549457d931d67949aab87d5430d906
zhukovka <helen.zhukova@gmail.com>
2017-07-20 01:28:13 +0300

    logger added

--- hoc/test/pure.test.js
+++ hoc/test/pure.test.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
+function printToLog(operation, firstValue, secondValue) {
+  console.log(operation, firstValue, secondValue);
+}
 function isNumber(value) {
   return typeof value === "number";
 }
 function mul(a, b) {
+  printToLog('умножение', a, b);
   if (isNumber(a) && isNumber(b)) {
     return a * b;
   } else {
@@ -10,38 +14,42 @@ function mul(a, b) {
   }
 }
 function add(a, b) {
+  printToLog('сложение', a, b);
   if (isNumber(a) && isNumber(b)) {
     return a + b;
   } else {
     throw new Error("Оба значения должны быть числами.");
-  } }
-  function div(a, b) {
-    if (isNumber(a) && isNumber(b)) {
-      return a / b;
-    } else {
-      throw new Error("Оба значения должны быть числами.");
-    }
   }
-  function sub(a, b) {
-    if (isNumber(a) && isNumber(b)) {
-      return a - b;
-    } else {
-      throw new Error("Оба значения должны быть числами.");
-    }
+}
+function div(a, b) {
+  printToLog('деление', a, b);
+  if (isNumber(a) && isNumber(b)) {
+    return a / b;
+  } else {
+    throw new Error("Оба значения должны быть числами.");
   }
-  describe('Pure functions', function() {
-    describe('type checking', function() {
-      it('should return 3', function() {
-        assert.equal(add(1, 2), 3);
-      });
-      it('should return 40', function() {
-        assert.equal(mul(10, 4), 40);
-      });
-      it('should return 2', function() {
-        assert.equal(div(10, 5), 2);
-      });
-      it('should return 3', function() {
-        assert.equal(add("1", 2), 3);
-      });
+}
+function sub(a, b) {
+  printToLog('вычетание', a, b);
+  if (isNumber(a) && isNumber(b)) {
+    return a - b;
+  } else {
+    throw new Error("Оба значения должны быть числами.");
+  }
+}
+describe('Pure functions', function() {
+  describe('type checking', function() {
+    it('should return 3', function() {
+      assert.equal(add(1, 2), 3);
+    });
+    it('should return 40', function() {
+      assert.equal(mul(10, 4), 40);
+    });
+    it('should return 2', function() {
+      assert.equal(div(10, 5), 2);
+    });
+    it('should return 3', function() {
+      assert.equal(add("1", 2), 3);
     });
   });
+});
