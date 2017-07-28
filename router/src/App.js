72fbe329c96d2dfcbc2a552188d932969636951d
zhukovka <helen.zhukova@gmail.com>
2017-07-28 15:24:58 +0300

    xhr added

--- router/src/App.js
+++ router/src/App.js
@@ -7,36 +7,49 @@ import Article from './components/Article.js';
 import ArticleGrid from './components/ArticleGrid.js';
 
 class App extends Component {
-  state = {
-    articles: [{
-      id: "1",
-      title: "Hello world",
-      content: "Hello react router!",
-      author: "Kukuruku"
-    }]
-  };
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.articles}/>
-        <nav>
-          <Link to="/">Все</Link>
-          <Link
-            to={{ pathname: '/', search: '?theme=economy' }}>
-            Экономика
-          </Link>
-          <Link
-            to={{ pathname: '/', search: '?theme=entertainment'}}>
-            Развлечения
-          </Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={ArticleGrid} />
-          <Route path="/article/:id" component={Article} />
-        </Switch>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    }; }
+    componentWillMount() {
+      const params = {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      };
+      fetch('/articles.json', params)
+      .then(response => response.json())
+      .then(articles => this.setState({ articles }));
+    }
+    render() {
+      const { articles } = this.state;
+      return (
+        <div className="App">
+          <Header data={articles}/>
+          <nav>
+            <Link to="/">Все</Link>
+            <Link
+              to={{ pathname: '/', search: '?theme=economy' }}>
+              Экономика
+            </Link>
+            <Link
+              to={{ pathname: '/', search: '?theme=entertainment'}}>
+              Развлечения
+            </Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" render={(props) => (
+              <ArticleGrid {...props} articles={articles} />
+            )}/>
+            <Route path="/article/:id" render={(props) => (
+              <Article {...props} articles={articles} />
+            )}/>
+          </Switch>
+        </div>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
--- router/src/components/ArticleGrid.js
+++ router/src/components/ArticleGrid.js
@@ -2,16 +2,9 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 class ArticleGrid extends Component{
-  state = {
-    articles: [{
-      id: "1",
-      title: "Hello world",
-      content: "Hello react router!"
-    }]
-  };
+
   render(){
-    let articles = this.state.articles;
-    let location = this.props.location;
+    let {location, articles} = this.props;
     if (location.query && location.query.theme) {
       articles = articles.filter(article => article.theme === location.query.theme);
     }
